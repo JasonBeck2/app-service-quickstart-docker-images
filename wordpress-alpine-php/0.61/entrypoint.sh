9030b554dfe3889baf269db05b70350709355c59
Jason Beck <jabec@microsoft.com>
2018-12-05 15:41:13 -0800

    Adding wordpress image push

--- wordpress-alpine-php/0.61/entrypoint.sh
+++ wordpress-alpine-php/0.61/entrypoint.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
 #set -e
-setup_mariadb_data_dir(){
-    test ! -d "$MARIADB_DATA_DIR" && echo "INFO: $MARIADB_DATA_DIR not found. creating ..." && mkdir -p "$MARIADB_DATA_DIR"
-
-    # check if 'mysql' database exists
-    if [ ! -d "$MARIADB_DATA_DIR/mysql" ]; then
-	    echo "INFO: 'mysql' database doesn't exist under $MARIADB_DATA_DIR. So we think $MARIADB_DATA_DIR is empty."
-	    echo "Copying all data files from the original folder /var/lib/mysql to $MARIADB_DATA_DIR ..."
-	    cp -R /var/lib/mysql/. $MARIADB_DATA_DIR
-    else
-	    echo "INFO: 'mysql' database already exists under $MARIADB_DATA_DIR."
-    fi
-
-    rm -rf /var/lib/mysql
-    ln -s $MARIADB_DATA_DIR /var/lib/mysql
-    chown -R mysql:mysql $MARIADB_DATA_DIR
-    test ! -d /run/mysqld && echo "INFO: /run/mysqld not found. creating ..." && mkdir -p /run/mysqld
-    chown -R mysql:mysql /run/mysqld
-}
-
-start_mariadb(){
-    /etc/init.d/mariadb setup
-    rc-service mariadb start 
-
-    rm -f /tmp/mysql.sock
-    ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock
-
-    # create default database 'azurelocaldb'
-    mysql -u root -e "CREATE DATABASE IF NOT EXISTS azurelocaldb; FLUSH PRIVILEGES;"
-}
+#setup_mariadb_data_dir(){
+#    test ! -d "$MARIADB_DATA_DIR" && echo "INFO: $MARIADB_DATA_DIR not found. creating ..." && mkdir -p "$MARIADB_DATA_DIR"
+#
+#    # check if 'mysql' database exists
+#    if [ ! -d "$MARIADB_DATA_DIR/mysql" ]; then
+#	    echo "INFO: 'mysql' database doesn't exist under $MARIADB_DATA_DIR. So we think $MARIADB_DATA_DIR is empty."
+#	    echo "Copying all data files from the original folder /var/lib/mysql to $MARIADB_DATA_DIR ..."
+#	    cp -R /var/lib/mysql/. $MARIADB_DATA_DIR
+#    else
+#	    echo "INFO: 'mysql' database already exists under $MARIADB_DATA_DIR."
+#    fi
+#
+#    rm -rf /var/lib/mysql
+#    ln -s $MARIADB_DATA_DIR /var/lib/mysql
+#    chown -R mysql:mysql $MARIADB_DATA_DIR
+#    test ! -d /run/mysqld && echo "INFO: /run/mysqld not found. creating ..." && mkdir -p /run/mysqld
+#    chown -R mysql:mysql /run/mysqld
+#}
+#
+#start_mariadb(){
+#    /etc/init.d/mariadb setup
+#    rc-service mariadb start 
+#
+#    rm -f /tmp/mysql.sock
+#    ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock
+#
+#    # create default database 'azurelocaldb'
+#    mysql -u root -e "CREATE DATABASE IF NOT EXISTS azurelocaldb; FLUSH PRIVILEGES;"
+#}
 
 #unzip phpmyadmin
-setup_phpmyadmin(){
-    test ! -d "$PHPMYADMIN_HOME" && echo "INFO: $PHPMYADMIN_HOME not found. creating..." && mkdir -p "$PHPMYADMIN_HOME"
-    cd $PHPMYADMIN_SOURCE
-    tar -xf phpMyAdmin.tar.gz -C $PHPMYADMIN_HOME --strip-components=1
-    cp -R phpmyadmin-config.inc.php $PHPMYADMIN_HOME/config.inc.php    
-    cp -R phpmyadmin-default.conf /etc/nginx/conf.d/default.conf
-	cd /
-    rm -rf $PHPMYADMIN_SOURCE
-    if [ ! $WEBSITES_ENABLE_APP_SERVICE_STORAGE ]; then
-        echo "INFO: NOT in Azure, chown for "$PHPMYADMIN_HOME  
-        chown -R www-data:www-data $PHPMYADMIN_HOME
-    fi 
-}    
+#setup_phpmyadmin(){
+#    test ! -d "$PHPMYADMIN_HOME" && echo "INFO: $PHPMYADMIN_HOME not found. creating..." && mkdir -p "$PHPMYADMIN_HOME"
+#    cd $PHPMYADMIN_SOURCE
+#    tar -xf phpMyAdmin.tar.gz -C $PHPMYADMIN_HOME --strip-components=1
+#    cp -R phpmyadmin-config.inc.php $PHPMYADMIN_HOME/config.inc.php    
+#    cp -R phpmyadmin-default.conf /etc/nginx/conf.d/default.conf
+#	cd /
+#    rm -rf $PHPMYADMIN_SOURCE
+#    if [ ! $WEBSITES_ENABLE_APP_SERVICE_STORAGE ]; then
+#        echo "INFO: NOT in Azure, chown for "$PHPMYADMIN_HOME  
+#        chown -R www-data:www-data $PHPMYADMIN_HOME
+#    fi 
+#}    
 
 setup_wordpress(){
 	test ! -d "$WORDPRESS_HOME" && echo "INFO: $WORDPRESS_HOME not found. creating ..." && mkdir -p "$WORDPRESS_HOME"
@@ -109,14 +109,14 @@ if [ "${DATABASE_TYPE}" == "local" ]; then
     echo 'mysql.default_socket = /run/mysqld/mysqld.sock' >> $PHP_CONF_FILE     
     echo 'mysqli.default_socket = /run/mysqld/mysqld.sock' >> $PHP_CONF_FILE     
     #setup MariaDB
-    echo "INFO: loading local MariaDB and phpMyAdmin ..."
-    echo "Setting up MariaDB data dir ..."
-    setup_mariadb_data_dir
-    echo "Setting up MariaDB log dir ..."
-    test ! -d "$MARIADB_LOG_DIR" && echo "INFO: $MARIADB_LOG_DIR not found. creating ..." && mkdir -p "$MARIADB_LOG_DIR"
-    chown -R mysql:mysql $MARIADB_LOG_DIR
-    echo "Starting local MariaDB ..."
-    start_mariadb
+    #echo "INFO: loading local MariaDB and phpMyAdmin ..."
+    #echo "Setting up MariaDB data dir ..."
+    #setup_mariadb_data_dir
+    #echo "Setting up MariaDB log dir ..."
+    #test ! -d "$MARIADB_LOG_DIR" && echo "INFO: $MARIADB_LOG_DIR not found. creating ..." && mkdir -p "$MARIADB_LOG_DIR"
+    #chown -R mysql:mysql $MARIADB_LOG_DIR
+    #echo "Starting local MariaDB ..."
+    #start_mariadb
 
     echo "Granting user for phpMyAdmin ..."
     # Set default value of username/password if they are't exist/null.
@@ -127,10 +127,10 @@ if [ "${DATABASE_TYPE}" == "local" ]; then
     echo "phpmyadmin password:" $DATABASE_PASSWORD
     echo "INFO: ++++++++++++++++++++++++++++++++++++++++++++++++++:"
     mysql -u root -e "GRANT ALL ON *.* TO \`$DATABASE_USERNAME\`@'localhost' IDENTIFIED BY '$DATABASE_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-    echo "Installing phpMyAdmin ..."
-    setup_phpmyadmin
-    echo "Loading phpMyAdmin conf ..."
-	load_phpmyadmin    
+    #echo "Installing phpMyAdmin ..."
+    #setup_phpmyadmin
+    #echo "Loading phpMyAdmin conf ..."
+	#load_phpmyadmin    
 fi
 
 # That wp-config.php doesn't exist means WordPress is not installed/configured yet.
