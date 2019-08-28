1fac2f83210c566e3da0ca0922c25f49f37b78ff
Jason Beck <jabec@microsoft.com>
2019-08-28 14:07:52 -0700

    Revert "nginx-fpm/php7.2.11"
    This reverts commit fd8faf0e3fd015ce3e466553f5a77bbd17ac7fd8.

--- nginx-fpm/php7.2.11/init_container.sh
+++ nginx-fpm/php7.2.11/init_container.sh
@@ -108,7 +108,3 @@ if test ! -e /home/LogFiles/nginx/error.log; then
     touch /home/LogFiles/nginx/error.log
 fi
 /usr/sbin/nginx -g "daemon off;"
-
-echo "Creating directories"
-mkdir /srv/www/shared
-mkdir /srv/www/shared/uploads
